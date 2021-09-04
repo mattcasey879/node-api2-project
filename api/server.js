ea91464ac3bbb575458a04b7a8733e9ef26656dc
Matt <matthew-casey@lambdastudents.com>
2021-09-03 19:53:48 -0500

    Finished MVP

--- api/server.js
+++ api/server.js
@@ -1,2 +1,15 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express')
+const postsRoutes = require("./posts/posts-router")
+const server = express()
+server.use(express.json())
+
+server.get("/" , (req, res) => {
+    res.send("From the server")
+})
+
+server.use("/api/posts", postsRoutes)
+
+
+module.exports = server
--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // require your server and launch it here
+const server = require("./api/server")
+
+server.listen(8000, () => {
+    console.log("Server running on http://localhost:8000");
+})
